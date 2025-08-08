3dae7c84068af73c6669c2ec7aca9e4e25554ded
Stuxint <160317550+Stuxint@users.noreply.github.com>
2025-08-08 15:44:03 +0400

    Add files via upload

---
+++ Steve Drawing Bot(v2).py
@@ -0,0 +1,282 @@
+#--------------MADE BOT WHICH CAN MAKE COLORING POSTER 4 ME!!!!!!!----------------------------------
+
+
+import pyautogui
+import time
+
+pyautogui.hotkey('win', 'run')
+time.sleep(0.1)
+pyautogui.typewrite('paint')
+time.sleep(3)
+pyautogui.hotkey('enter')
+
+time.sleep(5)
+pyautogui.moveTo(935,640)
+
+#------------DRAWING PART----------------------------
+#DRAWING FACE
+    #STEP 1
+time.sleep(2)
+pyautogui.dragTo(935,570)
+    #STEP 2
+time.sleep(0.1)
+pyautogui.dragTo(1015,570)
+    #STEP 3
+time.sleep(0.1)
+pyautogui.dragTo(1015,640)
+    #STEP 4
+time.sleep(0.1)
+pyautogui.dragTo(935,640)
+
+#DRAWING ARMS AND BODY
+time.sleep(0.1)
+pyautogui.moveTo(935,640)
+pyautogui.dragTo(895,640)
+
+pyautogui.dragTo(895,740)
+
+time.sleep(0.1)
+pyautogui.dragTo(925,740)
+
+time.sleep(0.1)
+pyautogui.dragTo(925,680)
+
+time.sleep(0.1)
+pyautogui.dragTo(935,680)
+
+time.sleep(0.1)
+pyautogui.dragTo(935,770)
+
+time.sleep(0.1)
+pyautogui.dragTo(1015,770)
+
+time.sleep(0.1)
+pyautogui.dragTo(1015,680)
+
+time.sleep(0.1)
+pyautogui.dragTo(1025,680)
+
+time.sleep(0.1)
+pyautogui.dragTo(1025,740)
+
+time.sleep(0.1)
+pyautogui.dragTo(1055,740)
+
+time.sleep(0.1)
+pyautogui.dragTo(1055,640)
+
+    #STEP 11
+time.sleep(0.1)
+pyautogui.dragTo(1015,640)
+
+#DRAWING SLEEVES
+time.sleep(0.1)
+pyautogui.moveTo(925,680)
+
+time.sleep(0.1)
+pyautogui.dragTo(895,680)
+
+time.sleep(0.1)
+pyautogui.moveTo(1025,680)
+
+time.sleep(0.1)
+pyautogui.dragTo(1055,680)
+
+#DRAWING LEGS
+time.sleep(0.1)
+pyautogui.moveTo(935,770)
+
+time.sleep(0.1)
+pyautogui.dragTo(935,840)
+
+time.sleep(0.1)
+pyautogui.dragTo(935,840)
+
+time.sleep(0.1)
+pyautogui.dragTo(1015,840)
+
+time.sleep(0.1)
+pyautogui.dragTo(1015,770)
+
+time.sleep(0.1)
+pyautogui.moveTo(935,820)
+
+time.sleep(0.1)
+pyautogui.dragTo(1015,820)
+
+time.sleep(0.1)
+pyautogui.moveTo(975,840)
+
+time.sleep(0.1)
+pyautogui.dragTo(975,800)
+
+#DRAWING NECK
+time.sleep(0.1)
+pyautogui.moveTo(945,640)
+
+time.sleep(0.1)
+pyautogui.dragTo(945,660)
+
+time.sleep(0.1)
+pyautogui.dragTo(955,660)
+
+time.sleep(0.1)
+pyautogui.dragTo(955,670)
+
+time.sleep(0.1)
+pyautogui.dragTo(995,670)
+
+time.sleep(0.1)
+pyautogui.dragTo(995,660)
+
+time.sleep(0.1)
+pyautogui.dragTo(1005,660)
+
+time.sleep(0.1)
+pyautogui.dragTo(1005,640)
+
+#DRAWING LEFT EYE
+time.sleep(0.1)
+pyautogui.moveTo(945,590)
+
+time.sleep(0.1)
+pyautogui.dragTo(945,610)
+
+time.sleep(0.1)
+pyautogui.dragTo(970,610)
+
+time.sleep(0.1)
+pyautogui.dragTo(970,590)
+
+time.sleep(0.1)
+pyautogui.dragTo(945,590)
+
+ #drawing line in eye
+time.sleep(0.1)
+pyautogui.moveTo(960,590)
+
+time.sleep(0.1)
+pyautogui.dragTo(960,610)
+
+#DRAWING RIGHT EYE
+time.sleep(0.1)
+pyautogui.moveTo(980,590)
+
+time.sleep(0.1)
+pyautogui.dragTo(980,610)
+
+time.sleep(0.1)
+pyautogui.dragTo(1003,610)
+
+time.sleep(0.1)
+pyautogui.dragTo(1003,590)
+
+time.sleep(0.1)
+pyautogui.dragTo(980,590)
+
+#DRAWING LINE
+time.sleep(0.1)
+pyautogui.moveTo(990,590)
+
+time.sleep(0.1)
+pyautogui.dragTo(990, 610)
+
+#DRAWING MOUTH
+time.sleep(0.1)
+pyautogui.moveTo(965,620)
+
+time.sleep(0.1)
+pyautogui.dragTo(965,630)
+
+time.sleep(0.1)
+pyautogui.dragTo(985,630)
+
+time.sleep(0.1)
+pyautogui.dragTo(985,620)
+
+time.sleep(0.1)
+pyautogui.dragTo(965,620)
+
+#DRAWING HAIR
+time.sleep(0.1)
+pyautogui.moveTo(935,620)
+
+time.sleep(0.1)
+pyautogui.dragTo(925,620)
+
+time.sleep(0.1)
+pyautogui.dragTo(925,550)
+
+time.sleep(0.1)
+pyautogui.dragTo(1025,550)
+
+time.sleep(0.1)
+pyautogui.dragTo(1025,620)
+
+time.sleep(0.1)
+pyautogui.dragTo(1015,620)
+
+#DRAWING BODY
+time.sleep(0.1)
+for x in range(19):
+    pyautogui.hotkey('tab')
+    time.sleep(0.1)
+pyautogui.hotkey('enter')
+
+#------------PAINTING PART----------------------------
+#pnt hair
+time.sleep(0.1)
+pyautogui.moveTo(1018,615)
+
+time.sleep(0.1)
+pyautogui.dragTo(1018,615)
+pyautogui.click()
+
+#pmt mouth
+time.sleep(0.1)
+pyautogui.moveTo(967,627)
+
+time.sleep(0.1)
+pyautogui.dragTo(967,627)
+pyautogui.click()
+
+
+#drawing I
+time.sleep(0.1)
+for x in range(23):
+    pyautogui.hotkey('tab')
+    time.sleep(0.1)
+pyautogui.hotkey('enter')
+
+time.sleep(0.1)
+pyautogui.moveTo(1040,825)
+
+time.sleep(0.1)
+pyautogui.dragTo(1040,845)
+
+#drawing M
+time.sleep(0.1)
+pyautogui.moveTo(1050,825)
+
+time.sleep(0.1)
+pyautogui.dragTo(1050,845)
+
+time.sleep(0.1)
+pyautogui.moveTo(1050,825)
+
+time.sleep(0.1)
+pyautogui.dragTo(1060,825)
+
+time.sleep(0.1)
+pyautogui.dragTo(1060,835)
+
+time.sleep(0.1)
+pyautogui.moveTo(1060,825)
+
+time.sleep(0.1)
+pyautogui.dragTo(1070,825)
+
+time.sleep(0.1)
+pyautogui.dragTo(1070,845)
+
+time.sleep(20)
